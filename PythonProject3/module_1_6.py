6567fad15cc7b5d918116d94f76cc7246752eaa8
АлтаевДмитрий <Baitog1982@gmail.com>
2024-12-04 19:56:20 +0800

    Add files via upload

---
+++ PythonProject3/module_1_6.py
@@ -0,0 +1,5 @@
+home_work=12
+hours_spent=1.5
+course='python'
+time_task=1.5/12
+print('Курс:',course,',''всего задач:',home_work,',''затрачено часов:',hours_spent,',''среднее время выполнения:',time_task,)
