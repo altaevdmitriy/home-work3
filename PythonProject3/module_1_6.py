e7f62594cc839256283b7ffc22298e7d6b3ba60b
АлтаевДмитрий <Baitog1982@gmail.com>
2024-12-04 21:50:06 +0800

    Update module_1_6.py

--- PythonProject3/module_1_6.py
+++ PythonProject3/module_1_6.py
@@ -1,5 +1,5 @@
 home_work=12
 hours_spent=1.5
 course='python'
-time_task=1.5/12
+time_task=hours_spent/hours_spent
 print('Курс:',course,',''всего задач:',home_work,',''затрачено часов:',hours_spent,',''среднее время выполнения:',time_task,)
